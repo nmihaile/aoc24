cd52dfadafcfe66a40d6e79bf5052928a4a34f9c
nmihaile <nmihaile@student.42heilbronn.de>
2024-12-12 23:05:27 +0100

    solution for aoc24-04-q2

--- 04/main.cpp
+++ 04/main.cpp
@@ -30,6 +30,15 @@ bool	search(Puzzle& puzzle, int x, int y, int dx, int dy, int w, int h, std::str
 	return (false);
 }
 
+int	search_mas(Puzzle& puzzle, int x, int y, int w, int h)
+{
+	if ( (search(puzzle, x-1,y-1, 1,1, w,h, std::string("MAS")) || search(puzzle, x+1,y+1, -1,-1, w,h, std::string("MAS")))
+			&&
+		 (search(puzzle, x-1,y+1, 1,-1, w,h, std::string("MAS")) || search(puzzle, x+1,y-1, -1,1, w,h, std::string("MAS"))) )
+		 	return (1);
+	return (0);
+}
+
 int	main(int ac, char **av)
 {
 	std::string		line;
@@ -54,22 +63,31 @@ int	main(int ac, char **av)
 	// print_puzzle(puzzle);
 
 	// Q1: count words in puzzle
-	size_t	count = 0;
+	size_t	count_q1 = 0;
 	int h = static_cast<int>(puzzle.size());
 	int w = static_cast<int>(puzzle[0].size());
 	for (int y = 0; y < h; ++y)
 		for (int x = 0; x < w; ++x)
 		{
-			count += search(puzzle, x, y, -1,  0, w, h, "XMAS");
-			count += search(puzzle, x, y,  1,  0, w, h, "XMAS");
-			count += search(puzzle, x, y,  0, -1, w, h, "XMAS");
-			count += search(puzzle, x, y,  0,  1, w, h, "XMAS");
-			count += search(puzzle, x, y, -1, -1, w, h, "XMAS");
-			count += search(puzzle, x, y,  1, -1, w, h, "XMAS");
-			count += search(puzzle, x, y, -1,  1, w, h, "XMAS");
-			count += search(puzzle, x, y,  1,  1, w, h, "XMAS");
+			count_q1 += search(puzzle, x, y, -1,  0, w, h, "XMAS");
+			count_q1 += search(puzzle, x, y,  1,  0, w, h, "XMAS");
+			count_q1 += search(puzzle, x, y,  0, -1, w, h, "XMAS");
+			count_q1 += search(puzzle, x, y,  0,  1, w, h, "XMAS");
+			count_q1 += search(puzzle, x, y, -1, -1, w, h, "XMAS");
+			count_q1 += search(puzzle, x, y,  1, -1, w, h, "XMAS");
+			count_q1 += search(puzzle, x, y, -1,  1, w, h, "XMAS");
+			count_q1 += search(puzzle, x, y,  1,  1, w, h, "XMAS");
 		}
-	std::cout << BLUE << "Answer 01: " << LIGHTYELLOW << BOLD << count << RESET << std::endl;
+	std::cout << BLUE << "Answer 01: " << LIGHTYELLOW << BOLD << count_q1 << RESET << std::endl;
+
+
+	// Q2: count crosed-MAS words in puzzle
+	size_t	count_q2 = 0;
+	for (int y = 0; y < h; ++y)
+		for (int x = 0; x < w; ++x)
+			count_q2 += search_mas(puzzle, x, y, w, h);
+	std::cout << BLUE << "Answer 02: " << LIGHTYELLOW << BOLD << count_q2 << RESET << std::endl;
+
 
 	return (0);
 }
