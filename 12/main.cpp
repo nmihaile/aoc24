092db873750b841711208304390465b3d2c2ca5d
nmihaile <nmihaile@student.42heilbronn.de>
2024-12-12 09:33:22 +0100

    adds aoc-util to aoc-12

--- 12/main.cpp
+++ 12/main.cpp
@@ -4,23 +4,9 @@
 #include <fstream>
 #include <vector>
 
-#define BG				"\033[48;5;4m"
-#define LIGHTRED		"\033[91m"
-#define LIGHTGREEN		"\033[92m"
-#define LIGHTYELLOW		"\033[93m"
-#define BLUE			"\033[34m"
-#define LIGHTBLUE		"\033[94m"
-#define LIGHTMAGENTA	"\033[95m"
-#define LIGHTCYAN		"\033[96m"
-#define RESET			"\033[0m"
-#define BOLD			"\033[1m"
+#include "aoc-util.hpp"
 
 typedef std::vector<std::vector<char>>	Garden;
-
-void	aocprint(std::string str)
-{
-	std::cout << str << std::endl;
-}
 
 void	print_garden(Garden garden)
 {
