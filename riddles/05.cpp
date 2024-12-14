bd9f1de0e67c2b7b32520be0e19eea02a8a1920d
nmihaile <nmihaile@student.42heilbronn.de>
2024-12-14 09:34:54 +0100

    solution aoc-05

--- riddles/05.cpp
+++ riddles/05.cpp
@@ -6,9 +6,40 @@
 
 #define INPUT "input/input05"
 
-typedef std::pair<int,int>				Rule;
-typedef std::vector<Rule>				Rules;
-typedef std::vector<std::vector<int>>	Updates;
+typedef std::pair<int,int>	Rule;
+typedef std::vector<Rule>	Rules;
+typedef std::vector<int>	Update;
+typedef std::vector<Update>	Updates;
+
+bool	is_valid_order(int& p1, int& p2, Rules& rules)
+{
+	for (Rule& rule : rules)
+		if (rule.first == p2 && rule.second == p1)
+			return (false);
+	return (true);
+}
+
+bool	is_valid_update(Update& update, Rules& rules)
+{
+	for (size_t i = 0; i < update.size() - 1; ++i)
+	{
+		if (!is_valid_order(update[i], update[i + 1], rules))
+			return (false);
+	}
+	return (true);
+}
+
+void	reorder(Update& update, Rules& rules)
+{
+	for (size_t i = 0; i < update.size() - 1; ++i)
+	{
+		if (!is_valid_order(update[i], update[i + 1], rules))
+		{
+			std::swap(update[i], update[i + 1]);
+			i = -1;
+		}
+	}
+}
 
 int	main(void)
 {
@@ -45,10 +76,28 @@ int	main(void)
 		}
 		++i;
 	}
-
 	fs.close();
+
+	
+	// q1 - check updates and sum middle page
+	// q2 - reorder invalid updates and sum middle page
+	size_t	q1_middle_pages = 0;
+	size_t	q2_middle_pages = 0;
+	for (Update& update : updates)
+	{
+		if (is_valid_update(update, rules))
+			q1_middle_pages += update[update.size() / 2];
+		else
+		{
+			reorder(update, rules);
+			q2_middle_pages += update[update.size() / 2];
+		}
+	}
+	std::cout << BLUE << "Answer 01: " << LIGHTYELLOW << BOLD << q1_middle_pages << RESET << std::endl;
+	std::cout << BLUE << "Answer 01: " << LIGHTYELLOW << BOLD << q2_middle_pages << RESET << std::endl;
 
 	return (0);
 }
 
 // clang++ -Wall -Wextra -Werror -std=c++11  main.cpp && ./a.out input
+// make 05
